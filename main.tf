139ad3ac30fd0afe1cf24f8e4017e12dd1f00a1b
RAHAMSHAIK <81603735+RAHAMSHAIK007@users.noreply.github.com>
2026-06-15 19:15:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "one" {
   count                  = 4
-  ami                    = "ami-0ecb62995f68bb549"
+  ami                    = "ami-0b6d9d3d33ba97d99"
   instance_type          = "c7i-flex.large"
   key_name               = "rahamdocker"
   vpc_security_group_ids = ["sg-0ddd6bb55cfb8039c"]
