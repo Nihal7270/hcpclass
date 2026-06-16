76f9901e9559c8308b539fac782226718798905f
Nihal Allugula <22uj1a6201@mrem.ac.in>
2026-06-16 11:58:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "one" {
   count                  = 4
   ami                    = "ami-01a00762f46d584a1"
   instance_type          = "c7i-flex.large"
-  key_name               = "practice"
+  key_name               = "jenkins-project"
   vpc_security_group_ids = ["sg-0bb52e16a8e2ac95d"]
   tags = {
     Name = var.instance_names[count.index]
